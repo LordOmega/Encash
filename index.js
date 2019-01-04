abf6332d3921fb87bfc0a9a474c5e1b89c291cdb
LordOmega <LordOmega@users.noreply.github.com>
2019-01-04 19:54:10 +0100

    v0

---
+++ index.js
@@ -0,0 +1,72 @@
+if(localStorage.getItem("data_encash") == undefined) {
+    localStorage.setItem("data_encash", JSON.stringify({
+        assets: {}
+    }));
+}
+
+var data = JSON.parse(localStorage.getItem("data_encash"));
+const element = document.getElementById('content');
+var _months_ = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+var _date_ = new Date();
+
+var React = {
+    createElement: function (tag, attrs, children) {
+        var e = document.createElement(tag);
+
+        for (var name in attrs) {
+            if (name && attrs.hasOwnProperty(name)) {
+                var v = attrs[name];
+                if (v === true) {
+                    e.setAttribute(name, name);
+                } else if (v !== false && v != null) {
+                    e.setAttribute(name, v.toString());
+                }
+            }
+        }
+
+        for (var i = 2; i < arguments.length; i++) {
+            var child = arguments[i];
+            if(child != null) {
+                e.appendChild(child.nodeType == null ? document.createTextNode(child.toString()) : child);
+            }             
+        }
+
+        return e;
+    }
+}
+
+function checkInput(input) {
+    return input != "" && input != null && input != undefined;
+}
+
+function calcCurrentDebt(asset) {
+    return (parseFloat(asset["debt"]) - ((asset["start_month"] - _date_.getMonth() + 1) * parseFloat(asset["monthly_payment"])));
+}
+
+function navAction() {
+    var asset = prompt("asset:", "");
+    if(checkInput(asset)) {
+        var debt = prompt("debt:", "");
+        if(checkInput(debt)) {
+            var monthlyPayment = prompt("monthly payment:", "");
+            if(checkInput(monthlyPayment)) {
+                var obj = {};
+                obj["asset"] = asset;
+                obj["debt"] = debt.replace(",", ".");
+                obj["monthly_payment"] = monthlyPayment.replace(",", ".");
+                obj["start_month"] = _date_.getMonth();
+                obj["start_year"] = _date_.getFullYear();
+
+                console.log(obj, asset, data.assets);
+
+                data.assets[asset] = obj;
+                document.getElementById('content').appendChild(React.createElement('tr',{},React.createElement('td',{style: "border-bottom: 1px solid rgb(247, 247, 248);"},asset + " - " + _months_[_date_.getMonth()]),React.createElement('td',{style: "border-bottom: 1px solid rgb(247, 247, 248);"}, calcCurrentDebt(obj) + '€')));
+                localStorage.setItem("data_encash", JSON.stringify(data));
+            }         
+        }
+    }
+}
+
+for (var key in data.assets) {
+    document.getElementById('content').appendChild(React.createElement('tr',{},React.createElement('td',{style: "border-bottom: 1px solid rgb(247, 247, 248);"},data.assets[key].asset + " - " + _months_[_date_.getMonth()]),React.createElement('td',{style: "border-bottom: 1px solid rgb(247, 247, 248);"}, calcCurrentDebt(data.assets[key]) + '€')));
+}
---
+++ sw.js
@@ -0,0 +1,50 @@
+var CACHE_NAME = 'Encash_Go_To_Zero_v0';
+var urlsToCache = [
+  '/Encash',
+  '/Encash/',
+  '/Encash/index.html',
+  '/Encash/index.css',
+  '/Encash/index.js',
+  'https://fonts.googleapis.com/css?family=Open+Sans:400,700'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith((function() {
+    console.log(event.request, urlsToCache.includes(event.request));
+    if(urlsToCache.includes(event.request)) {
+      return caches.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          caches.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      })
+    } else {
+      return fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    }   
+  }()));
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
